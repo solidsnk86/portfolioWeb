2bb152c3af7aadf7b87ae367d2251a581c699dac
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-08 22:58:49 -0300

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link"
+import { GithubIcon } from "lucide-react"
 
 export const Footer = () => {
 	const githubLink = [
-		{ name: '@solidSnk86', href: 'https://github.com/solidsnk86' }
+		{ str: 'seguir @solidSnk86', href: 'https://github.com/solidsnk86' }
 	]
 	return (
 		<div className='flex justify-center m-auto my-4'>
 			{
 				githubLink.map((i) => (
 					<Link
-					key={i.href}
-					href={i.href}
+						key={i.href}
+						href={i.href}
 					>
-					<span className='text-orange-400 text-sm hover:text-zinc-300 transition-all bg-gray-800 px-3 py-2 rounded-full'>{i.name}</span>
+						<span className='text-orange-400 text-sm hover:text-red-400 transition-all bg-gray-800 px-3 py-2 rounded-full hover:animate-pulse'>
+							<GithubIcon className="inline mx-1 text-zinc-300 w-5 h-5 mb-1" />
+							{i.str}
+						</span>
 					</Link>
 				))
 			}
