35e59df4a83721354de042f5ad64d0c90baa0b5e
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-09 22:49:49 -0300

    update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ export function Logo () {
 		<h1
 			className={`${interTight.className} mx-auto text-md font-black bg-clip-text text-transparent bg-gradient-to-r text-zinc-300 xl:pt-6 h-[120px] text-center min-w-[400px] text-[4rem] sm:text-[6rem] md:text-[6rem] lg:text-[10rem] xl:text-[8rem] hover:animate-pulse transition-[ease] hover:text-shadow-lg cursor-default`}
 		>
-			<span className='text-transparent [-webkit-text-stroke-width:4px] [-webkit-text-stroke-color:white]'>
+			<span className='text-transparent [-webkit-text-stroke-width:4px] [-webkit-text-stroke-color:#D4D4D8]'>
 				solid
 			</span>
 			Snk86
