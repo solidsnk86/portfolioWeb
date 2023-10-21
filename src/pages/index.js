e3150cded682b4d88662cdaaf7172311b9b87d2d
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-21 17:15:59 -0300

    up2

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,7 +106,7 @@ export default function Home() {
 						}
 					].map((item) => (
 						<div
-							className='flex flex-col text-zinc-300 items-center w-full sm:w-1/2 lg:w-1/3 xl:w-1/3 border-white border-opacity-10 border rounded-xl px-4 py-6 duration-200 hover:shadow-lg sm:hover:shadow-[#D4D4D8] hover:border-opacity-0 hover:bg-primary hover:bg-opacity-5 hover:scale-[1.05]'
+							className='flex flex-col text-zinc-300 items-center w-full sm:w-1/2 lg:w-1/3 xl:w-1/3 border-white border-opacity-10 border rounded-xl px-4 py-6 duration-200 hover:shadow-lg hover:shadow-[#D4D4D8] hover:border-opacity-0 hover:bg-primary hover:bg-opacity-5 hover:scale-[1.05]'
 							key={item.title}
 						>
 							<img className='w-20 sm:w-auto xl:w-32 sm:mr-0' src={item.logo} alt={item.title} />
