68b41433ef05f37338f246e98e6c22ad7bc3ef2f
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-21 13:49:09 -0300

    update

--- src/components/VideoReactIcon.js
+++ src/components/VideoReactIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const VideoReactIcon = () => {
   return (
     <div className='mt-20'>
-      <video width="140" height="260" autoPlay loop>
+      <video width="140" height="260" autoPlay loop muted>
         <source src="/img/videos/3d-react.mp4" type="video/mp4" />
         Tu navegador no admite la reproducción de video.
       </video>
