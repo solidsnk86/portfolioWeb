3b315ff74da7a169afa4c17c077b6745b657850c
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-23 22:53:29 -0300

    update media queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,6 +46,7 @@ export default function Home() {
 				<title>Calcagni Gabriel</title>
 				<meta name='description' content={description} />
 				<meta property='og:image' />
+				<meta property='author' content='solidSnk86' />
 				<meta property='twitter:image' content={ogImg} />
 				<meta property='og:title' content={title} />
 				<meta property='twitter:title' content={title} />
@@ -110,7 +111,7 @@ export default function Home() {
 							className='flex flex-col text-zinc-300 items-center w-full sm:w-1/2 lg:w-1/3 xl:w-1/3 border-white border-opacity-10 border rounded-xl px-4 py-6 duration-200 hover:shadow-lg hover:shadow-[#D4D4D8] hover:border-opacity-0 hover:bg-primary hover:bg-opacity-5 hover:scale-[1.05]'
 							key={item.title}
 						>
-							<img className='sm:w-auto xl:w-28 sm:mr-0' src={item.logo} alt={item.title} />
+							<img className='w-24 sm:w-24 md:w-24 lg:w-24 xl:w-28 sm:mr-0' src={item.logo} alt={item.title} />
 							<span className='font-bold mt-4 mb-2'>{item.title}</span>
 							<span className='text-sm mb-2 opacity-[.7]'>{item.description}</span>
 
