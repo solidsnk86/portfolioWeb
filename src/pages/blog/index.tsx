1764466a2ae5fea9c78b12a1e89117635f18a88a
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-11-17 05:16:06 -0300

    remove meteors background

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,7 +4,6 @@ import ImageAvatar from './AvatarBlog'
 import { ArrowLeft, ArrowRight, EyeIcon, Heart } from 'lucide-react'
 import Head from 'next/head'
 import { Card } from '@/components/Card'
-import { MT } from '@/components/MeteorLanguages'
 import { Footer } from '@/components/Footer'
 import { createClient } from '@supabase/supabase-js'
 import { BlogHeader } from '@/components/BlogHeader'
@@ -96,7 +95,10 @@ const MyBlog = () => {
 
 		const fetchPosts = async () => {
 			try {
-				const { data, error } = await supabase.from('posts').select()
+				const { data, error } = await supabase
+					.from('posts')
+					.select('article_id, user_id, name, company_dev, title, description, url, posted')
+					.order('posted', { ascending: false })
 
 				if (error) {
 					console.error('Error fetching posts:', error)
@@ -205,65 +207,64 @@ const MyBlog = () => {
 	}
 
 	return (
-		<>
-			<MT />
-			<main className='text-slate-100 bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0 h-[100%]'>
-				{preloaderVisible && <div className='spinner'></div>}
-				<Head>
-					<meta name='theme-color' content='#F05252' />
-				</Head>
-				<ArrowLeft
-					className='flex relative xl:fixed left-[14px] top-[1.6rem] cursor-pointer text-zinc-300 hover:opacity-[.8]'
-					onClick={() => window.open('/', '_self')}
-				/>
-				<BlogHeader />
-				{posts.map((post) => (
-					<div key={post.article_id} className='xl:w-1/2 justify-center mx-auto pt-20 px-3'>
-						<Card>
-							<article className='p-6 space-y-6 relative'>
-								<header>
-									<span className='text-zinc-400 text-sm'>{formatDate(post.posted)}</span>
-									<span className='text-zinc-400 absolute top-[25px] right-5 text-sm'>
-										<EyeIcon className='float-right mx-2 my-[2px] w-4 h-4' />
-										{articleViews[post.article_id] || 0}
-									</span>
-									<h1 className='text-2xl font-semibold my-2'>{post.title}</h1>
-									<p className='text-zinc-400 xl:line-clamp-none line-clamp-3'>
-										{post.description}
-									</p>
-								</header>
-								<aside className='flex space-x-3'>
-									<ImageAvatar user={post.user_id} />
-									<div
-										className='flex-col cursor-pointer'
-										onClick={() => window.open(post.url, '_blank')}
-									>
-										<span className='font-semibold'>{post.name}</span>
-										<p className='font-light text-zinc-600 text-sm'>{post.company_dev}</p>
-									</div>
-									<Link
-										href={post.url}
-										onClick={() => articleVisited(post.article_id)}
-										className='read-more absolute right-2 bottom-5 xl:right-10 xl:bottom-10 flex hover:underline'
-									>
-										Read More <ArrowRight className='mx-1 arrow' />
-									</Link>
-								</aside>
-								<button onClick={() => handleLike(post.article_id)}>
-									<Heart className='inline mx-2 hover:fill-red-500 transition-all hover:animate-pulse' />
-									{likes[post.article_id] ? likes[post.article_id].length : 0}
-								</button>
-								<button>
-									<ShareButton postTitle={post.title} postDescription={post.description} postUrl={post.url} />
-								</button>
-							</article>
-						</Card>
-					</div>
-				))}
-				<BlogForm newPost={newPost} setNewPost={setNewPost} sendPost={sendPost} />
-				<Footer />
-			</main>
-		</>
+		<main className='text-slate-100 bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0 h-[100%]'>
+			{preloaderVisible && <div className='spinner'></div>}
+			<Head>
+				<meta name='theme-color' content='#F05252' />
+			</Head>
+			<ArrowLeft
+				className='flex relative xl:fixed left-[14px] top-[1.6rem] cursor-pointer text-zinc-300 hover:opacity-[.8]'
+				onClick={() => window.open('/', '_self')}
+			/>
+			<BlogHeader />
+			{posts.map((post) => (
+				<div key={post.article_id} className='xl:w-1/2 justify-center mx-auto pt-20 px-3'>
+					<Card>
+						<article className='p-6 space-y-6 relative'>
+							<header>
+								<span className='text-zinc-400 text-sm'>{formatDate(post.posted)}</span>
+								<span className='text-zinc-400 absolute top-[25px] right-5 text-sm'>
+									<EyeIcon className='float-right mx-2 my-[2px] w-4 h-4' />
+									{articleViews[post.article_id] || 0}
+								</span>
+								<h1 className='text-2xl font-semibold my-2'>{post.title}</h1>
+								<p className='text-zinc-400 xl:line-clamp-none line-clamp-3'>{post.description}</p>
+							</header>
+							<aside className='flex space-x-3'>
+								<ImageAvatar user={post.user_id} />
+								<div
+									className='flex-col cursor-pointer'
+									onClick={() => window.open(post.url, '_blank')}
+								>
+									<span className='font-semibold'>{post.name}</span>
+									<p className='font-light text-zinc-600 text-sm'>{post.company_dev}</p>
+								</div>
+								<Link
+									href={post.url}
+									onClick={() => articleVisited(post.article_id)}
+									className='read-more absolute right-2 bottom-5 xl:right-10 xl:bottom-10 flex hover:underline'
+								>
+									Read More <ArrowRight className='mx-1 arrow' />
+								</Link>
+							</aside>
+							<button onClick={() => handleLike(post.article_id)}>
+								<Heart className='inline mx-2 hover:fill-red-500 transition-all hover:animate-pulse' />
+								{likes[post.article_id] ? likes[post.article_id].length : 0}
+							</button>
+							<button>
+								<ShareButton
+									postTitle={post.title}
+									postDescription={post.description}
+									postUrl={post.url}
+								/>
+							</button>
+						</article>
+					</Card>
+				</div>
+			))}
+			<BlogForm newPost={newPost} setNewPost={setNewPost} sendPost={sendPost} />
+			<Footer />
+		</main>
 	)
 }
 
