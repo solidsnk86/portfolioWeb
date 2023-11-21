2d5eb66fd5885b72dac72ff4fabc47cecc6ab597
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-11-21 00:17:48 -0300

    update

--- src/components/BlogHeader.tsx
+++ src/components/BlogHeader.tsx
@@ -1,6 +1,6 @@
 export const BlogHeader = () => {
 	return (
-		<header className='text-slate-100 flex flex-col justify-center m-auto pt-10 xl:w-1/2 space-y-3 p-12'>
+		<header className='text-slate-100 flex flex-col justify-center m-auto p-6 pt-12 xl:w-1/2 space-y-3'>
 			<h1 className='text-4xl font-bold text-shadow-md shadow-slate-100'>Welcome to my Blog!</h1>
 			<hr className='border-zinc-600' />
 			<p>
@@ -8,7 +8,7 @@ export const BlogHeader = () => {
 				your projects featured on this blog or even the stuff you consider intrest!. It's currently
 				in the development and testing phase. Your participation would be greatly appreciated!
 			</p>
-			<ul className="list-disc pt-6 m-8 underline underline-offset-4">
+			<ul className="pt-6">
 				<li className="text-zinc-400">
 					Latest Posts:
 				</li>
