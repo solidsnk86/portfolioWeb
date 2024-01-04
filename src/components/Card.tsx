57225edcf69828b5ecd6c78035270ffc5eff31a2
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-01-04 07:18:00 -0300

    update bg gradient to b-l

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,7 +15,7 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
 		mouseX.set(clientX - left)
 		mouseY.set(clientY - top)
 	}
-	const maskImage = useMotionTemplate`radial-gradient(240px at ${mouseX}px ${mouseY}px, white, transparent)`
+	const maskImage = useMotionTemplate`radial-gradient(340px at ${mouseX}px ${mouseY}px, white, transparent)`
 	const style = { maskImage, WebkitMaskImage: maskImage }
 
 	return (
@@ -24,9 +24,9 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
 			className='overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600 '
 		>
 			<div className='pointer-events-none'>
-				<div className='absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]' />
+				<div className='absolute inset-0 z-0 transition duration-1000 [mask-image:linear-gradient(black,transparent)]' />
 				<motion.div
-					className='absolute inset-0 z-10  bg-gradient-to-br opacity-100 via-red-500/50  transition duration-1000 group-hover:opacity-50 '
+					className='absolute inset-0 z-10 bg-gradient-to-bl opacity-100 via-red-500/50 transition duration-1000 group-hover:opacity-50 '
 					style={style}
 				/>
 				<motion.div
