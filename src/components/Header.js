16cc9c1d984c336ac9cdd094bbfe79598d143927
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-01-12 07:56:06 -0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ import { useTranslation } from 'react-i18next'
 import { Language } from 'tabler-icons-react'
 
 export function Header() {
-	const [isHovered, setIsHovered] = useState(false)
 	const { t, i18n } = useTranslation()
 
 	useEffect(() => {
