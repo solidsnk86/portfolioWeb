43c779397ba5d278566d28e3afdede8bde0d3bc1
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-01 10:40:03 -0300

    update

--- src/pages/table/index.tsx
+++ src/pages/table/index.tsx
@@ -23,36 +23,6 @@ export const VisitData = () => {
 
 	return (
 		<section className='xl:p-16 xl:flex mx-auto xl:justify-center text-zinc-100 h-full'>
-			<div className='flares'>
-				<img
-					src='img/flare-top.png'
-					loading='lazy'
-					sizes='(max-width: 1920px) 100vw, 1920px'
-					srcSet='img/flare-top-500.png 500w, img/flare-top-800.png 800w, img/flare-top-1080.png 1080w, img/flare-top.png 1920w'
-					alt=''
-					className='flares-top w-full h-full'
-				/>
-				<img
-					src='img/flare-right.png'
-					loading='lazy'
-					sizes='(max-width: 1622px) 100vw, 1622px'
-					srcSet='img/flare-right-500.png 500w, img/flare-right-800.png 800w, img/flare-right-1080.png 1080w, img/flare-right.png 1622w'
-					alt=''
-					className='new-flare-nav top-right hide-mobile w-full h-full'
-				/>
-				<img
-					src='img/flare-left.png'
-					loading='lazy'
-					sizes='(max-width: 1518px) 100vw, 1518px'
-					srcSet='img/flare-left-500.png 500w, img/flare-left-800.png 800w, img/flare-left.png 1518w'
-					alt=''
-					className='new-flare-nav top-left hide-mobile w-full h-full'
-				/>
-			</div>
-
-
-
-
 			<table className='border-zinc-800 border z-50'>
 				<thead className='border-zinc-800 border text-justify'>
 					<tr className='head-table'>
