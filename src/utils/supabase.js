ff9db69dfbd0ee22b1e697788f714111b6205a44
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-14 09:47:37 -0300

    update dep

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+import { config } from 'dotenv'
+config()
+
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
