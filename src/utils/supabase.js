b6755b1d4d939e504fc3d74ea763c8638bb58465
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-14 08:08:14 -0300

    update .env variables to vercel

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
+import { error } from 'console'
 
-const SUPABASE_URL = 'https://wbywikatpjrneagwppxf.supabase.co'
-const SUPABASE_API_KEY =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndieXdpa2F0cGpybmVhZ3dwcHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk4Mzg5MzAsImV4cCI6MjAxNTQxNDkzMH0.nv6KxxPZBSiROB3-bak4LGAud2ex-wCDvyykMrYDCZQ'
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY
+
+if (!SUPABASE_URL || !SUPABASE_API_KEY) {
+	throw new Error('Mmmm 🛴', error)
+}
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY)
