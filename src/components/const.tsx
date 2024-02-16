ef0f1e56e950e7d3e11ba8cd47836a6e23142893
Gabriel Calcagni <93176365+solidsnk86@users.noreply.github.com>
2024-02-16 15:09:51 -0300

    Update const.tsx

--- src/components/const.tsx
+++ src/components/const.tsx
@@ -4,7 +4,7 @@ import { Inter, Inter_Tight as InterTight } from 'next/font/google'
 
 export const title = 'Portfolio Calcagni Gabriel'
 export const description = '¡Ey, pásate y echa un vistazo a mi portfolio!'
-export const ogImg = '/images/og/og-img.png'
+export const ogImg = '/img/og/og-img.png'
 
 export const csvData = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_URL
 export const geolocation = 'https://geolocation.microlink.io'
