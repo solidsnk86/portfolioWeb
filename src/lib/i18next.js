746a16d31ba1789e2e5744d25484f161278d7e93
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-16 17:50:26 -0300

    update

--- src/lib/i18next.js
+++ src/lib/i18next.js
@@ -31,8 +31,7 @@ i18n.use(initReactI18next).init({
 				titlePost: 'Post Title',
 				githubUser: 'Github user ID',
 				postDescription: 'Description about your post..',
-				urlPost: 'Your site URL',
-				twitterButton: 'Follow me'
+				urlPost: 'Your site URL'
 			}
 		},
 		es: {
@@ -62,8 +61,7 @@ i18n.use(initReactI18next).init({
 				titlePost: 'Título de la publicación',
 				githubUser: 'Usuario de Github',
 				postDescription: 'Descripción acerca de tu publicación..',
-				urlPost: 'La URL de tu sitio',
-				twitterButton: 'Sígueme'
+				urlPost: 'La URL de tu sitio'
 			}
 		}
 	},
