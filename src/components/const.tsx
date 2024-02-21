1cac41d57662b2b70d676344da05a8bcf9df9cdb
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-21 17:36:46 -0300

    change favicon

--- src/components/const.tsx
+++ src/components/const.tsx
@@ -1,5 +1,3 @@
-import { renderToString } from 'react-dom/server'
-import { MyIcon } from './icons/MyIcon'
 import { Inter, Inter_Tight as InterTight } from 'next/font/google'
 
 export const title = 'Portfolio Calcagni Gabriel'
@@ -9,7 +7,7 @@ export const ogImg = 'https://raw.githubusercontent.com/solidsnk86/portfolioWeb/
 export const csvData = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_URL
 export const geolocation = 'https://geolocation.microlink.io'
 
-export const favicon = renderToString(<MyIcon />)
+export const favicon = 'https://github.com/solidsnk86/CV_GEC/blob/master/solidsnk86.png?raw=true'
 
 export const inter = Inter({ weight: ['400', '500', '600', '700', '900'], subsets: ['latin'] })
 export const interTight = InterTight({ weight: ['500', '800', '900'], subsets: ['greek'] })
