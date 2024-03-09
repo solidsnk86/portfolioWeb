3a22b069e1744d349d12f41e27038cdd0d2a78d6
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-03-09 08:47:04 -0300

    updates now

--- src/components/GithubStats.js
+++ src/components/GithubStats.js
@@ -50,7 +50,7 @@ export const GithubStats = ({ repoName }) => {
 	}, [repoName])
 
 	return (
-		<div className='inline-flex items-center space-x-1' title={t(`navLink3`)}>
+		<div className='inline-flex items-center space-x-1' title={t('navLink3')}>
 			<a
 				className='text-xs hover:brightness-150 hover:no-underline'
 				href={`https://github.com/solidsnk86/${repoName}/`}
