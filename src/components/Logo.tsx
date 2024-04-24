dd2795997667be3237509a33ae8830888f5b4115
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-04-24 04:47:27 -0300

    update tailwind styles and css

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,9 +3,9 @@ import { interTight } from '../components/const'
 export function Logo() {
 	return (
 		<h1
-			className={`${interTight.className} mx-auto text-md font-black bg-clip-text text-transparent bg-gradient-to-r text-zinc-300 xl:pt-6 h-[120px] text-center min-w-[370px] text-[3.6rem] sm:text-[6rem] md:text-[6rem] lg:text-[10rem] xl:text-[8rem] transition-[ease] hover:text-shadow-lg cursor-default`}
+			className={`${interTight.className} mx-auto text-md font-black bg-clip-text text-transparent bg-gradient-to-r text-zinc-300 xl:pt-6 h-[120px] text-center min-w-[370px] text-[3.6rem] sm:text-[6rem] md:text-[6rem] lg:text-[10rem] xl:text-[8rem] transition-[ease] cursor-default`}
 		>
-			<span className='text-transparent relative bottom-[2px] [-webkit-text-stroke-width:4px] [-webkit-text-stroke-color:#D4D4D8]'>
+			<span className='text-transparent hover:text-[#ffa600ab] hover:[-webkit-text-stroke-width:2px] duration-500 transition-colors relative bottom-[2px] [-webkit-text-stroke-width:4px] [-webkit-text-stroke-color:#D4D4D8]'>
 				solid
 			</span>
 			Snk86
