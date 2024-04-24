06960db267eee5cf93e470264392d7f486ce63fe
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-04-24 08:54:44 -0300

    update disableZoom in portable devices

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import disableZoom from '../components/ZoomDisable.js'
+import { useEffect } from 'react'
 
 export default function Document() {
+	useEffect(() => {
+		disableZoom()
+	}, [])
+
 	return (
 		<Html lang='es-ES'>
 			<Head />
