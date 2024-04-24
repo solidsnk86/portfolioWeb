69c615a6a6ae14f6e45e0083ee5cab6518a6bc83
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-04-23 23:47:42 -0300

    update and delete file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
-import React from 'react'
 import { I18nextProvider } from 'react-i18next'
 import i18n from '../lib/i18next'
+import React from 'react'
 
 const App = ({ Component, pageProps }) => {
 	return (
