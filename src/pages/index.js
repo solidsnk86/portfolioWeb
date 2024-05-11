38ad2fe098cc34575d60d06516e90c3e9bec470e
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-10 23:28:16 -0300

    update changes and remove icon comp

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,15 +91,18 @@ export default function Home() {
 				<ArrowUpRight className='inline w-4 h-4' />
 			</Link>
 			<Footer />
-			{
-				[
-					{ repoName: 'portfolioWeb' }
-				].map((repo) => (
-					<div key={repo.repoName} className='text-zinc-200 p-1 text-[10px] rounded-md my-6 justify-center mx-auto flex bg-zinc-600/30 w-fit border border-zinc-700 update-dot cursor-default'>
-						<p className='ml-1'>{t('update')} <GithubDescription className='text-[10px] mx-1' repoName={repo.repoName} /><GitBranch className='inline mb-[2px] mx-[2px] w-4 h-4' /></p>
-					</div>
-				))
-			}
+			{[{ repoName: 'portfolioWeb' }].map((repo) => (
+				<div
+					key={repo.repoName}
+					className='text-zinc-200 p-1 text-[10px] rounded-md my-6 justify-center mx-auto flex bg-zinc-600/30 w-fit border border-zinc-700 update-dot cursor-default'
+				>
+					<p className='ml-1'>
+						{t('update')}{' '}
+						<GithubDescription className='text-[10px] mx-1' repoName={repo.repoName} />
+						<GitBranch className='inline mb-[2px] mx-[2px] w-4 h-4' />
+					</p>
+				</div>
+			))}
 			<Visit />
 			<p className='text-center text-zinc-600 text-xs pb-2'>{t('last_footer')}</p>
 		</>
