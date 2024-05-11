9c4573c9c16298e1b88637f718100ce076dce25e
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-10 22:18:29 -0300

    update disable zoom function

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import disableZoom from '../components/ZoomDisable.js'
-import { useEffect } from 'react'
 
 export default function Document() {
-	useEffect(() => {
-		try {
-			disableZoom()
-		} catch (error) {
-			throw new Error(error)
-		}
-	}, [])
-
 	return (
 		<Html lang='es-ES'>
 			<Head />
