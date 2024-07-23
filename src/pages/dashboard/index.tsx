01b6a049d42b670b4b2481a75e19bbea43e8b13f
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-07-23 08:12:29 -0300

    feat: update favicon

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,12 +2,14 @@ import { FetchPost } from '@/components/FetchPosts'
 import { Footer } from '@/components/Footer'
 import { PostSender } from '@/components/PostSender'
 import Head from 'next/head'
+import { favicon } from '@/components/const'
 
 export default function Post() {
 	return (
 		<>
 			<Head>
 				<title>SolidSnk86 - Dashboard</title>
+				<link rel='shortcut icon' href={favicon} type='image/x-icon' />
 			</Head>
 			<main className='m-4'>
 				<PostSender />
